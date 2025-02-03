ea32b9a82c8f3c0153989b536c1e8a179d466693
KIM TAEHYUN <tanggu01@connect.hku.hk>
2025-02-03 20:01:55 +0800

    TicketSeller 캡슐화

--- chapter01/src/main/java/org/eternity/theater/step01_refactor/Theater.java
+++ chapter01/src/main/java/org/eternity/theater/step01_refactor/Theater.java
@@ -8,15 +8,16 @@ public class Theater {
     }
 
     public void enter(Audience audience) {
-        if (audience.getBag().hasInvitation()) {
-            Ticket ticket = ticketSeller.getTicketOffice().getTicket();
-            audience.getBag().setTicket(ticket);
-        } else {
-            Ticket ticket = ticketSeller.getTicketOffice().getTicket();
-            audience.getBag().minusAmount(ticket.getFee());
-            ticketSeller.getTicketOffice().plusAmount(ticket.getFee());
-            audience.getBag().setTicket(ticket);
-        }
+        ticketSeller.sellTo(audience); // 아래 세부 구현을 TicketSeller 안으로 이동
+//        if (audience.getBag().hasInvitation()) {
+//            Ticket ticket = ticketSeller.getTicketOffice().getTicket();
+//            audience.getBag().setTicket(ticket);
+//        } else {
+//            Ticket ticket = ticketSeller.getTicketOffice().getTicket();
+//            audience.getBag().minusAmount(ticket.getFee());
+//            ticketSeller.getTicketOffice().plusAmount(ticket.getFee());
+//            audience.getBag().setTicket(ticket);
+//        }
     }
 }
 
